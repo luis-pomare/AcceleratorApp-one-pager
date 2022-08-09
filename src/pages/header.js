ae80b09a40fcb2f73a6432b0329090c09067ee6c
unknown <luispomare92@gmail.com>
2022-08-09 01:01:06 -0500

    Add header

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,14 +1,19 @@
 import React from "react"
+import * as style from "../styles/header.module.css"
 
 export default function Header() {
   return (
     <header>
-      <h2>Join in and connect</h2>
-      <h1>To faster your innovation potential</h1>
-      <p>
+      <h2 className={style.titleOne}>Join in and connect</h2>
+      <h1 className={style.titleTwo}>To faster your innovation potential</h1>
+      <p className={style.paragraph}>
         AcceleratorApp Innovation Community — Tools and community to facilitate
         innovation between all members of our community
       </p>
+      <button type="button" className={style.headerButton}>
+        Schedule a Demo
+      </button>
+      <img src="/Video.svg" alt="video logo" />
     </header>
   )
 }
